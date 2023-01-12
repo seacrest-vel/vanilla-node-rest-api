5c442979c15eb93a970031e03d8edef540001498
Seacrest <paganseacrest@gmail.com>
2023-01-12 15:29:47 +0200

    composed conditions

--- server.js
+++ server.js
@@ -2,27 +2,16 @@ const http = require("http")
 const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require("./controllers")
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/api/products" && req.method === "GET") {
-    getProducts(req, res)
+  if (req.url === "/api/products") {
+    req.method === "GET" && getProducts(req, res)
+    req.method === "POST" && createProduct(req, res)
   } 
   
-  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "GET") {
+  else if (req.url.match(/\/api\/products\/([0-9]+)/)) {
     const id = req.url.split("/")[3]
-    getProduct(req, res, id)
-  } 
-  
-  else if (req.url === "/api/products" && req.method === "POST") {
-    createProduct(req, res)
-  } 
-  
-  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "PUT") {
-    const id = req.url.split("/")[3]
-    updateProduct(req, res, id)
-  } 
-  
-  else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "DELETE") {
-    const id = req.url.split("/")[3]
-    deleteProduct(req, res, id)
+    req.method === "GET" && getProduct(req, res, id)
+    req.method === "PUT" && updateProduct(req, res, id)
+    req.method === "DELETE" && deleteProduct(req, res, id)
   } 
   
   else {
@@ -31,7 +20,6 @@ const server = http.createServer((req, res) => {
     })
     res.end(JSON.stringify({message: "Not found"}))
   }
-    
 })
 
 const PORT = process.env.PORT || 3000
